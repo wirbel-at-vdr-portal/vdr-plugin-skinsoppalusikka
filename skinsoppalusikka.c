fd90a417d152b80d59026ecf97675d8fdcbaca19
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2017-06-24 23:35:27 +0300

    Updated for vdr-2.3.7.

--- skinsoppalusikka.c
+++ skinsoppalusikka.c
@@ -14,8 +14,8 @@
 #include "setup.h"
 #include "soppalusikka.h"
 
-#if defined(APIVERSNUM) && APIVERSNUM < 20301
-#error "VDR-2.3.1 API version or greater is required!"
+#if defined(APIVERSNUM) && APIVERSNUM < 20307
+#error "VDR-2.3.7 API version or greater is required!"
 #endif
 
 #ifndef GITVERSION
